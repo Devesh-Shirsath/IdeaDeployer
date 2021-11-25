1fba68746b332586e97f4ab165a44e22f606202d
JyotiRajdixit <jyotirajdixit50@gmail.com>
2021-11-25 20:07:20 +0530

    first commit landing page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar';
+import Sidebar from './components/sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sidebar.js
@@ -0,0 +1,29 @@
+import React,{useState,useEffect} from "react";
+
+function Sidebar() {
+    return (
+        <aside id="sidebar" className="sidebar">
+
+        <ul className="sidebar-nav" id="sidebar-nav">
+        
+          <li className="nav-heading">Pages</li>
+          <li className="nav-item">
+            <a className="nav-link " href="index.html">
+              <i className="bi bi-grid"></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+        
+          <li className="nav-item">
+            <a className="nav-link collapsed" href="pages-contact.html">
+              <i className="bi bi-envelope"></i>
+              <span>Contact</span>
+            </a>
+          </li>
+        </ul>
+        
+        </aside>
+  );
+}
+
+export default Sidebar;
