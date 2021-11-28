ba63f5b0ad6ab46695d09fb5c20b50d4f53f2cf1
JyotiRajdixit <jyotirajdixit50@gmail.com>
2021-11-28 23:09:18 +0530

    card changes and side nav

--- src/components/card.js
+++ src/components/card.js
@@ -8,32 +8,31 @@ function Card() {
           <div className="card">
 
             <div className="filter">
+              <i className="icon bi bi-heart"></i>
               <a className="icon" href="#" data-bs-toggle="dropdown"><i className="bi bi-three-dots"></i></a>
               <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li className="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-
-                <li><a className="dropdown-item" href="#">Today</a></li>
-                <li><a className="dropdown-item" href="#">This Month</a></li>
-                <li><a className="dropdown-item" href="#">This Year</a></li>
+                <li><a className="dropdown-item" href="#"><i className="bi bi-pencil"></i>Edit</a></li>
+                <li><a className="dropdown-item" href="#"><i className="bi bi-trash"></i>Delete</a></li>
               </ul>
             </div>
 
             <div className="card-body">
-              <h5 className="card-title">Reports <span>/Today</span></h5>
+              <h5 className="card-title">Challenge Title  <span className="text-danger small pt-1 fw-bold"> • Live</span> <span> <br />Creater • Challenge • time left • 200 ideas</span></h5>
 
               <div className="d-flex align-items-center">
-                <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                  <i className="bi bi-currency-dollar"></i>
-                </div>
-                <div className="ps-3">
-                  <h6>$3,264</h6>
-                  <span className="text-success small pt-1 fw-bold">8%</span> <span className="text-muted small pt-2 ps-1">increase</span>
-
+                <div className="ps-1">
+                  <span className="text-muted small pt-2">
+                    This is the description of the challenge posted by Jyotiraj Dixit. This is the dummy text.
+                    This is the description of the challenge posted by Jyotiraj Dixit. This is the dummy text.
+                  </span>
                 </div>
               </div>
 
+              <div className="d-flex" style={{justifyContent:"flex-end"}}>
+                <div>
+                  <button type="button" class="btn btn-light">Light</button>
+                </div>
+              </div>
             </div>
 
           </div>
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// import firebase from "firebase/app";
+// import "firebase/database";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAjZHzdlR_rkttSuZ634u-_PRAHLrNH45M",
+//     authDomain: "planner-crud-79575.firebaseapp.com",
+//     projectId: "planner-crud-79575",
+//     storageBucket: "planner-crud-79575.appspot.com",
+//     messagingSenderId: "431610005823",
+//     appId: "1:431610005823:web:b096f4bbaf95cc6cc26b8a"
+//   };
+
+// const fireDb = firebase.initializeApp(firebaseConfig);
+
+// export default fireDb.database().ref();
